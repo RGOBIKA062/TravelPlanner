38d6a37a4815fe5db054ffa05262100ed1d78285
RGOBIKA062 <gobikarr@gmail.com>
2025-04-27 21:56:30 +0530

    changes made

---
+++ mini proj()/travel_planner/models/Contact.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    subject: String,
+    message: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
